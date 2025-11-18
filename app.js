6066413bc5472e00d2de8a2824a383c3a8bcfcd8
unknown <Administrador@UNI-LABF-7448.fuvs.br>
2025-11-18 20:59:40 -0300

    arrumado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello DevOPS World, Protect the main branch!");
+  res.status(200).send("Hello DevOps World, Protect the main branch!");
 });
 
 module.exports = app;
